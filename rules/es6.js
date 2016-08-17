e271b37564a655c5bf2d8df53af78db1ecc06222
George Papadakis <phaistonian@gmail.com>
2016-08-17 10:19:49 +0300

    Comma on

--- rules/es6.js
+++ rules/es6.js
@@ -38,7 +38,7 @@ module.exports = {
     // https://github.com/eslint/eslint/blob/master/docs/rules/arrow-spacing.md
     'arrow-spacing': [1, { 'before': true, 'after': true }],
     // require trailing commas in multiline object literals
-    'comma-dangle': 2,
+    'comma-dangle': [1, "always"],
     // verify super() callings in constructors
     'constructor-super': 1,
     // enforce the spacing around the * in generator functions
