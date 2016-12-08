3ca1f8298f9492ff90b1b65d06b0fba942a49b67
Eleni <eepitropaki@phaistosnetworks.gr>
2016-12-08 11:46:45 +0200

    removed capitalize-comments rule

--- rules/style.js
+++ rules/style.js
@@ -158,6 +158,6 @@ module.exports = {
     // Enforce spacing between rest and spread operators and their expressions
     'rest-spread-spacing': [1],
     // Enforce or disallow capitalization of the first letter of a comment
-    'capitalize-comments': 0
+    //'capitalize-comments': 0
   }
 };
