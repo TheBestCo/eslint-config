27a7c68d6640212a81ba0c2026b7c49c74387edd
George Papadakis <phaistonian@gmail.com>
2017-05-09 10:30:50 +0300

    Fixes

--- rules/react.js
+++ rules/react.js
@@ -160,7 +160,9 @@ module.exports = {
     'react/self-closing-comp': WARN,
     // Enforce spaces before the closing bracket of self-closing JSX elements
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-space-before-closing.md
-    'react/jsx-space-before-closing': [WARN, 'always'],
+    'react/jsx-tag-spacing': [WARN, {
+        'beforeSelfClosing': true,
+    }],
     // Enforce component methods order
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
     'react/sort-comp': [WARN, {
